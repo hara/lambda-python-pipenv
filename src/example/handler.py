afe2dc9de61cffff95066dfc91791b558601e620
Yoshihiro Hara <hara.yoshihiro@gmail.com>
2018-04-02 22:57:06 +0900

    fix(handler): use PYTHONPATH

--- src/example/handler.py
+++ src/example/handler.py
@@ -1,10 +1,3 @@
-import sys
-import os
-example_dir = os.path.dirname(os.path.abspath(__file__))
-src_dir = os.path.dirname(example_dir)
-vendor_dir = os.path.join(src_dir, 'vendor')
-sys.path.append(vendor_dir)
-
 import requests
 
 
